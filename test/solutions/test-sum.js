cf936dcc5b536f1772c7b3bfebe7351a1640a4f9
sbx_user1059 <sbx_user1059@ip-10-11-74-46.eu-west-2.compute.internal>
2018-02-21 14:00:13 +0000

    Wed Feb 21 14:00:13 UTC 2018

--- test/solutions/test-sum.js
+++ test/solutions/test-sum.js
@@ -2,6 +2,8 @@ var sum = require('../../lib/solutions/sum');
 
 exports['compute sum'] = function (test) {
     test.equal(sum(1, 2), 3);
-    test.throws(sum(-5, 1));
+    test.throws(function () { sum(-5, 1) }, Error, 'first number not OK');
+    test.throws(function () { sum(5, -1) }, Error, 'second number not OK');
+    test.throws(function () { sum(105, 106) }, Error, 'numbers not OK');
     test.done();
 };
